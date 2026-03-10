7f4a423ecbfb8cf1358f19cf43906d224cda8bf5
DwijTak <dwij13.tak2008@gmail.com>
2026-03-10 14:07:23 +0530

    UC5 - Inline array initialization for banner lines

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,26 +1,28 @@
 /**
- * OOPSBannerApp UC4 – Render OOPS as Banner using String Array and Loop
+ * OOPSBannerApp UC5 – Render OOPS as Banner using Inline Array Initialization
  *
- * This use case improves UC3 by storing banner lines in a String array
- * and iterating through them using a for-each loop.
+ * This use case extends UC4 by defining and initializing
+ * the banner lines array in a single statement using String.join().
  *
  * @author Developer
- * @version 4
+ * @version 5.0
  */
 
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        String[] lines = new String[7];
+        String[] lines = {
 
-        lines[0] = String.join(" ", " *** ", " *** ", " ***** ", " ***** ");
-        lines[1] = String.join(" ", "*   *", "*   *", "*   * ", "*     ");
-        lines[2] = String.join(" ", "*   *", "*   *", "*   * ", "*     ");
-        lines[3] = String.join(" ", "*   *", "*   *", "***** ", " **** ");
-        lines[4] = String.join(" ", "*   *", "*   *", "*     ", "    * ");
-        lines[5] = String.join(" ", "*   *", "*   *", "*     ", "    * ");
-        lines[6] = String.join(" ", " *** ", " *** ", "*     ", "***** ");
+            String.join(" ", " *** ", " *** ", " ***** ", " ***** "),
+            String.join(" ", "*   *", "*   *", "*   * ", "*     "),
+            String.join(" ", "*   *", "*   *", "*   * ", "*     "),
+            String.join(" ", "*   *", "*   *", "***** ", " **** "),
+            String.join(" ", "*   *", "*   *", "*     ", "    * "),
+            String.join(" ", "*   *", "*   *", "*     ", "    * "),
+            String.join(" ", " *** ", " *** ", "*     ", "***** ")
+
+        };
 
         for (String line : lines) {
             System.out.println(line);
